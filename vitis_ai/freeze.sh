ee37eab3a96a1e7c3feb4cb591fc2fcf593bd80f
DebarajRana <66633242+debarajrana85@users.noreply.github.com>
2025-08-26 02:06:18 +0530

    Update freeze.sh

--- vitis_ai/freeze.sh
+++ vitis_ai/freeze.sh
@@ -1,1 +1,27 @@
+#!/bin/bash
 
+freeze() 
+{
+  freeze_graph \
+    --input_graph	./build/checkpoints/infer_graph.pb \
+    --input_meta_graph	./build/checkpoints/float_model.ckpt.meta \
+    --input_checkpoint	./build/checkpoints/float_model.ckpt \
+    --output_graph	./build/freeze/frozen_graph.pb \
+    --output_node_names	$OUT_NODE \
+    --input_binary	true
+}
+
+
+####################################################################################
+echo "-----------------------------------------"
+echo "START FREEZING THE GRAPH.."
+echo "-----------------------------------------"
+
+rm -rf ./build/freeze
+mkdir -p ./build/freeze
+
+freeze 2>&1 | tee ./build/logs/freeze.log
+
+echo "-----------------------------------------"
+echo "FREEZE GRAPH COMPLETED"
+echo "-----------------------------------------"
